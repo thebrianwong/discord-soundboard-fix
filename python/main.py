1cbd6eaefc6c8db217b8fb044885052739bdcf61
Brian Wong <brianw7218@gmail.com>
2024-05-20 21:26:18 -0700

    Initial commit (considered PyAutoGUI, now trying Puppeteer)

---
+++ python/main.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# pyautogui.click("./dock_google_chrome.png")
+# pyautogui.click("./postman_send_button.png")
+# pyautogui.click("./postman_sidebar.png")
+pyautogui.locateOnWindow()
+print("found it")
